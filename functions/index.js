92e558ef22fb821b8247403bda21d5a36520d621
sebastian-alejandro <sebastian.alejandro.hv@gmail.com>
2020-01-03 10:58:55 -0300

    added createPost function

--- functions/index.js
+++ functions/index.js
@@ -10,16 +10,24 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.getPosts = functions.https.onRequest((request, response) => {
     admin.firestore().collection('posts').get()
         .then(data => {
+
             let posts = [];
+
             data.forEach(doc => {
                 posts.push(doc.data());
             });
+
             return response.json(posts);
         })
         .catch((err) => console.error(err));
 })
 
 exports.createPost = functions.https.onRequest((request, response) => {
+
+    if (request.method !== 'POST') {
+        return response.status(400).json({ error: 'Method not allowed' });
+    }
+
     const newPost = {
         body: request.body.body,
         userHandle: request.body.userHandle,
@@ -30,6 +38,10 @@ exports.createPost = functions.https.onRequest((request, response) => {
         .collection('posts')
         .add(newPost)
         .then(doc => {
-            response.json({ message: `document`})
+            response.json({ message: `document ${doc.id} created successfully`});
+        })
+        .catch(err => {
+            response.status(500).json({error: 'Something went wrong!!'});
+            console.error(err);
         })
 })
