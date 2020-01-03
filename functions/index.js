601d3f517c6e49a22bbdac2b5391d2d4ec5d243e
sebastian-alejandro <sebastian.alejandro.hv@gmail.com>
2020-01-03 16:47:20 -0300

    added firebase auth

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,44 @@ const app = express();
 
 admin.initializeApp();
 
+const firebaseConfig = {
+    apiKey: "AIzaSyC9Nm1M9NrVZxjfXabhcAdu2-6_6w0IpQM",
+    authDomain: "socialapp-263614.firebaseapp.com",
+    databaseURL: "https://socialapp-263614.firebaseio.com",
+    projectId: "socialapp-263614",
+    storageBucket: "socialapp-263614.appspot.com",
+    messagingSenderId: "950398256620",
+    appId: "1:950398256620:web:ddd69752394d8ff0e06cc9"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+app.post('/signup', (request, response) => {
+
+    const newUser = {
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword,
+        handle: request.body.handle,
+    }
+
+    // TODO: validate data
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return response
+                .status(201)
+                .json({
+                    message: `user ${data.user.uid} signed up successfully`
+                })
+        })
+        .catch((err) => {
+            console.error(err);
+            return response.status(500).json({ error: err.code });
+        })
+});
+
 app.get('/posts', (request, response) => {
     admin
         .firestore()
