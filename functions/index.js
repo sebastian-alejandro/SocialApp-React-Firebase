b0d453b7d72b359939f10ac954e693cc889c281a
sebastian-alejandro <sebastian.alejandro.hv@gmail.com>
2020-01-14 14:44:43 -0300

    added empty validation in reg. fields

--- functions/index.js
+++ functions/index.js
@@ -18,6 +18,17 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.match(emailRegEx)) return true;
+    else return false;
+}
+
 app.post('/signup', (request, response) => {
 
     const newUser = {
@@ -27,6 +38,19 @@ app.post('/signup', (request, response) => {
         name: request.body.name,
     }
 
+    let errors = {};
+
+    if (isEmpty(newUser.email)) {
+        errors.email = 'Email must not be empty';
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address';
+    }
+
+    if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if (isEmpty(newUser.name)) errors.name = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return response.status(400).json(errors);
     // TODO: validate data
 
     let token, userId;
