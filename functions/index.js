c9a8f094b3d502b54c2c1ae1617c0d3dfc26be2b
sebastian-alejandro <sebastian.alejandro.hv@gmail.com>
2020-01-20 13:44:18 -0300

    done login endpoint

--- functions/index.js
+++ functions/index.js
@@ -87,6 +87,36 @@ app.post('/signup', (request, response) => {
 
 });
 
+app.post('/login', (request, response) => {
+
+    const user = {
+        email: request.body.email,
+        password: request.body.password
+    };
+
+    let errors = {};
+
+    if (isEmpty(user.email)) errors.email = 'Must not be empty';
+    if (isEmpty(user.password)) errors.password = 'Must not be empty';
+
+    if (Object.keys(errors).length > 0) return response.status(400).json(errors);
+
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            return response.json({token});
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/wrong-password') {
+                return response.status(403).json({ general: 'Wrong credentials, please try again'});
+            } else return response.status(500).json({error: err.code})
+        })
+
+});
+
 app.get('/posts', (request, response) => {
     db.collection('posts')
         .orderBy('createdAt', 'desc')
